56432c09be5e37151ff3064282998d34c7214bac
Elias Simojoki <simo.simojoki@gmail.com>
2026-06-08 23:05:51 +0300

    feat: add brightnessctl

--- home.nix
+++ home.nix
@@ -32,6 +32,8 @@
 
     pkgs.python3
 
+    pkgs.brightnessctl
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
