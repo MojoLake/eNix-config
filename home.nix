e2f274cf47a2b46e8052adf389763468f28eb78e
Elias Simojoki <simo.simojoki@gmail.com>
2026-06-27 12:18:06 +0300

    feat: add codex to home-manager

--- home.nix
+++ home.nix
@@ -41,6 +41,8 @@
 
     pkgs.jq
 
+    pkgs.codex
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
