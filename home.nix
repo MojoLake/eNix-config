17fc2514838273a36f405e83151db2e0a1f10670
Elias Simojoki <simo.simojoki@gmail.com>
2026-07-03 11:10:11 +0300

    feat: add anki

--- home.nix
+++ home.nix
@@ -60,6 +60,8 @@
 
     pkgs.thunderbird
 
+    pkgs.anki
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
