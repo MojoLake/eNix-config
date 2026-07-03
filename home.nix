90ffe5e6ec822851a2f5f1504b4315043df268bd
Elias Simojoki <simo.simojoki@gmail.com>
2026-07-03 17:03:19 +0300

    feat: add imv and zathura

--- home.nix
+++ home.nix
@@ -64,6 +64,9 @@
 
     pkgs.xdg-utils
 
+    pkgs.imv
+    pkgs.zathura
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
